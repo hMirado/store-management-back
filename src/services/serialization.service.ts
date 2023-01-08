8d35638d8b0765cfb6cf91a4477e317bce1495eb
Hobiniaina Mirado RAMAHATSANGIARISON <miradotsangy@gmail.com>
2023-01-08 22:53:16 +0300

    format response

--- src/services/serialization.service.ts
+++ src/services/serialization.service.ts
@@ -38,7 +38,7 @@ export const getSerializationByProductShop = async (productId: number, shopId: n
     condition = {shop_id: shopId}
   }
   try {
-    return await model.Serialization.findAll(
+    const serializations: typeof model.Serialization =  await model.Serialization.findAll(
       {
         include: [
           {
@@ -57,6 +57,36 @@ export const getSerializationByProductShop = async (productId: number, shopId: n
         ]
       }
     )
+    let serializationsGrouped: any[] = [];
+    serializations.forEach((serialization: typeof model.Serialization ) => {
+      serializationsGrouped.push(
+        {
+          serialization_id: serialization.serialization_id,
+          serialization_uuid: serialization.serialization_uuid,
+          serialization_value: serialization.serialization_value,
+          attribute_serialization: serialization.attribute_serialization,
+          createdAt: serialization.createdAt,
+          updatedAt: serialization.updatedAt,
+          fk_product_id: serialization.fk_product_id,
+          fk_serialization_type_id: serialization.fk_serialization_type_id,
+          fk_shop_id: serialization.fk_shop_id,
+          serialization_type_label: serialization.serialization_type.label
+        }
+      )
+    })
+    const grouped = serializationsGrouped.reduce((group: typeof model.Serialization, serialization: typeof model.Serialization) => {
+      const { attribute_serialization } = serialization;
+      group[attribute_serialization] = group[attribute_serialization] ?? [];
+      group[attribute_serialization].push(serialization);
+      return group
+    }, {})
+
+    let results = [];
+    for (const key in grouped) {
+      results.push(grouped[key])
+    }
+
+    return results;
   } catch (error: any) {
     console.log('\nserialization.servie::getProductSerialization');
     console.log(error);
